1fdfe89138049d45596018f578aef73a2ae5af6f
Allan Ramos <allangrds@gmail.com>
2025-06-13 08:55:45 -0300

    feat(aulas/2-pacotes-importantes): add classe #5 busca cep

--- aulas/1-fundacao/6-slices/main.go
+++ aulas/1-fundacao/6-slices/main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-  meuArray := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray), cap(meuArray), meuArray)
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:0]), cap(meuArray[:0]), meuArray[:0])
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:2]), cap(meuArray[:2]), meuArray[:2])
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[2:]), cap(meuArray[2:]), meuArray[2:])
+	meuArray := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+	// o len ele retorna o tamanho do slice
+	// o cap ele retorna a capacidade do slice
+	// o slice é uma fatia de um array, ele é um ponteiro para o array
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray), cap(meuArray), meuArray)
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:0]), cap(meuArray[:0]), meuArray[:0])
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:2]), cap(meuArray[:2]), meuArray[:2])
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[2:]), cap(meuArray[2:]), meuArray[2:])
 
-  meuArray = append(meuArray, 22)
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:2]), cap(meuArray[:2]), meuArray[:2])
-  fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray), cap(meuArray), meuArray)
+	meuArray = append(meuArray, 22)
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray[:2]), cap(meuArray[:2]), meuArray[:2])
+	fmt.Printf("len=%d cap=%d array=%v\n", len(meuArray), cap(meuArray), meuArray)
 }
---
+++ aulas/2-pacotes-importantes/5-busca-cep/main.go
@@ -0,0 +1,62 @@
+repackage main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	// os é um pacote que fornece uma interface para interagir com o sistema operacional
+	// os.Args é um slice de strings que contém os argumentos da linha de comando
+	// o 1: é para ignorar o primeiro argumento, que é o nome do programa
+	for _, cep := range os.Args[1:] {
+		var url = "http://viacep.com.br/ws/" + cep + "/json/"
+
+		req, err := http.Get(url)
+		if err != nil {
+			//Estou usando o Fprintf que recebe como primeiro argumento o Writer, que nesse caso é o Stderr
+			//O Stderr é um pacote que fornece uma interface para escrever na saída de erro padrão
+			fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
+		}
+		defer req.Body.Close()
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler resposta: %v\n", err)
+		}
+
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da response: %v\n", err)
+		}
+
+		file, err := os.Create("cep.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Logradouro: %s\n", data.Cep, data.Logradouro))
+
+	}
+}
